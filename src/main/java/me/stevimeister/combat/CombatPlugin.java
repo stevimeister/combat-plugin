9b84c7bef9a466b9544f8ae05b4234c5ad5254ea
Mikolaj <39224663+stevimeister@users.noreply.github.com>
2021-02-10 12:49:58 +0000

    Wprowadzono powiadomienie o aktualizacji wtyczki

--- src/main/java/me/stevimeister/combat/CombatPlugin.java
+++ src/main/java/me/stevimeister/combat/CombatPlugin.java
@@ -1,8 +1,18 @@
 package me.stevimeister.combat;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import me.stevimeister.combat.configuration.impl.MainConfiguration;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 /**
  * @author stevimeister on 05/01/2021
@@ -10,6 +20,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class CombatPlugin extends JavaPlugin {
 
     private static CombatPlugin instance;
+
+    private Gson gson;
 
     private Metrics metrics;
     private MainConfiguration mainConfiguration;
@@ -22,9 +34,24 @@ public final class CombatPlugin extends JavaPlugin {
         this.metrics = new Metrics(this, 9894);
         this.mainConfiguration = new MainConfiguration();
         this.combatManager = new CombatManager();
+        this.gson = new Gson();
 
         new CombatTask(this);
         new CombatListeners(this);
+
+        this.checkVersion(this.getDescription().getName(), this.getDescription().getVersion());
+    }
+
+    public void checkVersion(final String pluginName, final String pluginVersion) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/stevimeister/" + pluginName + "/main/version.json").openStream()))) {
+            final String onlineVersion = this.gson.fromJson(bufferedReader.lines().collect(Collectors.joining()), JsonObject.class).get("version").getAsString();
+            if (Objects.equals(pluginVersion, onlineVersion))
+                return;
+
+            this.getLogger().log(Level.WARNING, "Wtyczka której używasz jest nieaktualna! Pobierz nową wersje z https://github.com/stevimeister/" + pluginName + "/releases/download/" + onlineVersion + "/" + pluginName + "-" + onlineVersion);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static CombatPlugin getInstance() {
